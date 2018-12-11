ef8e68d61df2f321705b3394d803107139ace6ac
alufers <alufers@wp.pl>
2018-12-11 20:05:34 +0100

    CHanged paths, added real patrons proxy with minification

---
+++ patrons.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/alufers/owm-patrons/common"
+
+	"github.com/google/go-github/v19/github"
+	"golang.org/x/oauth2"
+)
+
+type J map[string]interface{}
+
+var config = common.PopulatedConfig
+
+func writeJSON(w http.ResponseWriter, code int, data interface{}) {
+	w.WriteHeader(code)
+	serialized, err := json.Marshal(data)
+	if err != nil {
+		serialized, _ = json.Marshal(err.Error())
+	}
+	w.Write(serialized)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_API_KEY")},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	fileContent, _, _, err := client.Repositories.GetContents(ctx, config.RepoAuthor, config.RepoName, config.PatronsFilePath, &github.RepositoryContentGetOptions{
+		Ref: "develop",
+	})
+
+	if err != nil {
+		fmt.Println("An error has occured fetching patrons.json contents", err.Error())
+		writeJSON(w, 500, J{
+			"message": "An error has occured fetching patrons.json contents",
+		})
+		return
+	}
+	jsonData, err := fileContent.GetContent()
+	if err != nil {
+		panic(err)
+	}
+	patrons := J{}
+	err = json.Unmarshal([]byte(jsonData), &patrons)
+	if err != nil {
+		fmt.Println("Failed to parse patrons.json", err.Error())
+		writeJSON(w, 500, J{
+			"message": "Failed to parse patrons.json",
+		})
+		return
+	}
+
+	writeJSON(w, 200, patrons)
+	//client.Repositories()
+}
